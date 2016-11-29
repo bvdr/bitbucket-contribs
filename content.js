03eb707dcb289433c7f49eb847e56fc02dee8179
Bogdan Dragomir <bogdanvdragomir@gmail.com>
2016-11-29 18:55:18 +0200

    add code in files types: .json,.js,.scss,.css,.map,.svg,.php removed code in files types: .json,.scss,.css,.map,.js,.php

--- content.js
+++ content.js
@@ -1,2 +1,2 @@
 console.log("qujbi")
-console.log("cygnk")
+console.log("plwvs")
--- content.php
+++ content.php
@@ -11,5 +11,5 @@
         echo "stuii";
         echo "hhrau";
         echo "uarvi";
-        echo "iuady";
+        echo "aipfg";
 ?>
