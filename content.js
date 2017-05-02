0b8a76e6b80637ed87fe3d536a5845c06d4f3d82
Bogdan Dragomir <bogdanvdragomir@gmail.com>
2017-05-02 15:21:57 +0300

    add code in files types: .js,.scss,.css,.map,.php removed code in files types: .js,.css,.map,.php

--- content.js
+++ content.js
@@ -1,2 +1,2 @@
 console.log("qujbi")
-console.log("eatrk")
+console.log("dwhej")
--- content.php
+++ content.php
@@ -13,5 +13,5 @@
         echo "uarvi";
         echo "lygdq";
         echo "ijcgq";
-        echo "rpdfn";
+        echo "hdymt";
 ?>
